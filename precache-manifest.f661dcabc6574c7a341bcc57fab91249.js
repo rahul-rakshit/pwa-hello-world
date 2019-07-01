8d1f067b0443a21da6dd1202d5a31327b51c57da
Rahul Rakshit <rahul.rakshit@posteo.net>
2019-07-01 15:06:31 +0100

    Updates

---
+++ precache-manifest.f661dcabc6574c7a341bcc57fab91249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716420a743280acf9f491c9b4e7ac1f1",
+    "url": "/pwa-hello-world/index.html"
+  },
+  {
+    "revision": "860cbcbde4a61fd3918b",
+    "url": "/pwa-hello-world/static/css/main.38d01169.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/pwa-hello-world/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "860cbcbde4a61fd3918b",
+    "url": "/pwa-hello-world/static/js/main.1c269d1e.chunk.js"
+  },
+  {
+    "revision": "6dd2e480a2b439e39364",
+    "url": "/pwa-hello-world/static/js/runtime~main.869c722a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52b5598b111dd3cbc70f5fee83d9201f.js"
+  "/pwa-hello-world/precache-manifest.f661dcabc6574c7a341bcc57fab91249.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pwa-hello-world/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
