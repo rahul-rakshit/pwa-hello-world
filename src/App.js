88ca0cb64be33efee5bca9b24f4d14f10eba768e
Rahul Rakshit <rahul.rakshit@posteo.net>
2019-07-01 14:40:59 +0100

    Implement basic application

--- src/App.js
+++ src/App.js
@@ -2,7 +2,42 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  const [color, setColor] = React.useState("white");
+
+  return (
+    <React.Fragment>
+      <div className="heading-container">
+        <div className="heading">What color</div>
+        <div className="heading">do you want</div>
+        <div className="heading">the box to be?</div>
+      </div>
+
+      <div className="button-container">
+        <button className="button orange" onClick={() => setColor("orange")}>
+          Orange
+        </button>
+        <button className="button aqua" onClick={() => setColor("aqua")}>
+          Aqua
+        </button>
+        <button className="button khaki" onClick={() => setColor("khaki")}>
+          Khaki
+        </button>
+        <button className="button tomato" onClick={() => setColor("tomato")}>
+          Tomato
+        </button>
+        <button className="button skyblue" onClick={() => setColor("skyblue")}>
+          Skyblue
+        </button>
+        <button className="button hotpink" onClick={() => setColor("hotpink")}>
+          Hotpink
+        </button>
+      </div>
+
+      <div className="colored-box-container">
+        <div className="colored-box" style={{ backgroundColor: color }} />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
